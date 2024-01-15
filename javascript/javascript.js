5dcfb850080c87c4ac2e7ab07ad66fd11a1a260f
unknown <sandroads@yahoo.com>
2024-01-15 20:04:12 -0300

    atualização

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,18 @@
 
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.prenventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');//togle: add caso não tenha, remove caso tenha.
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    }else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
